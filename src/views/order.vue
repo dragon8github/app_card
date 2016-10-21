cf2d3dadd1037d601bc6630de2858545ec9f80da
dragon8github <928532756@qq.com>
2016-10-21 16:14:15 +0800

    总算是实现了。但有瑕疵啊

--- src/views/order.vue
+++ src/views/order.vue
@@ -178,11 +178,15 @@
 		    Velocity(el.querySelector("._content"),{translateX: "0%"},350,"ease");
 		    Velocity(el.querySelector("._title"),{translateX: "0%",opacity:1},350,"ease");
 			Velocity(el.querySelector("._leftIco,_rightIco"),{opacity:1},100); 
-
+			done();  //神坑，如果不加上这个的话，路由不会停止。一直保持在class-enter-active状态
 		},
 		leave: function (el, done) {
-		  	Velocity(el.querySelector("._content"),{translateX: "100%"},350,"ease");
-		  	
+		  	 Velocity(el.querySelector("._content"),{translateX: "100%"},350,"ease");
+		  	 Velocity(el.querySelector("._title"),{translateX: "50%",opacity:0},350,"ease");
+		     Velocity(el.querySelector("._leftIco,_rightIco"),{opacity:0},350,"ease");
+		  	 setTimeout(()=> {
+		  	 	done();  //神坑，如果不加上这个的话，路由不会停止。一直保持在class-leave-active状态,并且不会消失
+		  	 },200)
 		}
  	},
  	components:{
